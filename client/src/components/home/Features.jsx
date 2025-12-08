dcafa8b8d72ea076958029b211793a4e5247eb9c
Tushar Pathare <patharetushar10@gmail.com>
2025-12-08 15:02:26 +0530

    UI update: added Navbar, CallToAction, Footer and enhanced Home, Login, Layout, and Dashboard pages

--- client/src/components/home/Features.jsx
+++ client/src/components/home/Features.jsx
@@ -7,6 +7,7 @@ const Features = () => {
     <div
       className="flex flex-col items-center my-10 scroll-mt-12"
       id="features"
+      style={{marginBottom:"350px"}}
     >
       <div className="flex items-center gap-2 text-sm text-green-800 bg-green-400/10 rounded-full px-6 py-1.5 mb-10">
         <Zap width={14} />
